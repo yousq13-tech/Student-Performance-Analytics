32ddb4d9f6db12c02bdd1abebea0d448e2649f63
yousq13-tech <yousq13@gmail.com>
2026-01-04 21:12:10 +0400

    Create performance_analysis.py

---
+++ performance_analysis.py
@@ -0,0 +1,15 @@
+marks = [50, 55, 58]
+threshold = 60
+total = 0
+count = 0
+
+for mark in marks:
+    total += mark
+    count += 1
+
+average = total / count
+
+if average < threshold:
+    print("Student is At Risk")
+else:
+    print("Student Performance is Good")
